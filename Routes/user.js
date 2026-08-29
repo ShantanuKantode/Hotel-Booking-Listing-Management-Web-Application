d4fdf9a85c86f94515551b796603ad0bd33f19a6
Shantanu Kantode <shantanukantode2@gmail.com>
2026-08-29 19:29:50 +0530

    Login Routes Created

--- Routes/user.js
+++ Routes/user.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const router= express.Router();
+const User = require("../models/user");
+const wrapAsync = require('../utils/wrapAsync');
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
+
+router.post("/signup" , wrapAsync(async(req,res)=>{
+    try{
+   const{username,email,password} = req.body;
+   const newUser = User({username,email});
+   const newRegister = await User.register(newUser,password);
+   req.flash("success","Welcome");
+   res.redirect("/listings");
+   }
+    catch(e){
+        req.flash("error",e.message);
+        res.redirect("/signup");
+    }
+  
+}));
+
+router.get('/login' , (req,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login" , 
+    passport.authenticate("local" ,{
+        failureRedirect : "/login",
+        failureFlash : true,
+    }),
+    async(req,res)=>{
+        req.flash("success","Welcome ! You are loggeed in !");
+        res.redirect("/listings");
+    }
+);
+
+
 
 
 module.exports = router;
